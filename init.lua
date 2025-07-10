e4eb734981226524833c6b66670879b42cb7999a
onecattwo <Tyler.H.Chow@gmail.com>
2025-07-09 21:20:09 -0600

    sound plays on player position

--- init.lua
+++ init.lua
@@ -3,5 +3,6 @@ ModRegisterAudioEventMappings("mods/bruh_sound_game_over/GUIDs.txt")
 
 function OnPlayerDied( player_entity )
     print("Bruh sound effect mod: Player has died, playing sound.")
-    GamePlaySound( "mods/bruh_sound_game_over/bruh.bank", "bruh/bruh_sound",0,0 )
+    local x,y = EntityGetTransform(player_entity)
+    GamePlaySound( "mods/bruh_sound_game_over/bruh.bank", "bruh/bruh_sound",x,y )
 end
